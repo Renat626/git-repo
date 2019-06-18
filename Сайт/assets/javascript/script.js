4fc311bf4a1604ab55d89752344db7d30db70e13
Slogur <ugadchicow44@gmail.com>
2019-06-18 16:18:08 +0500

    Новый плагин

--- Сайт/assets/javascript/script.js
+++ Сайт/assets/javascript/script.js
@@ -2,11 +2,15 @@ function answer() {
 	var ans = $("#inp").val();
 	if (ans == "Корова из-за угла выглядывает." || ans == "Корова из-за угла выглядывает" || ans == "Корова из за угла выглядывает." || ans == "Корова из за угла выглядывает" || ans == "корова из-за угла выглядывает" || ans == "корова из за угла выглядывает" || ans == "корова из-за угла выглядывает." || ans == "корова из за угла выглядывает.") {
 		$("#quest").text("Ответ правильный!");
-		$("body").css("background-color", "royalblue");
+		$("body").animate({
+			backgroundColor: "royalblue"
+		}, 500);
 	}
 	else {
 		$("#quest").text("Ответ неправильный!"); 
-		$("body").css("background-color", "red");
+		$("body").animate({
+			backgroundColor: "red"
+		}, 500);
 	}
 	$("#inp").val("");
 }
