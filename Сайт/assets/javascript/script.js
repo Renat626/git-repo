1b0179a440b53fedc0abae80b5b546c7f775c38f
Slogur <ugadchicow44@gmail.com>
2019-06-18 16:19:49 +0500

    Порезал

--- Сайт/assets/javascript/script.js
+++ Сайт/assets/javascript/script.js
@@ -2,15 +2,11 @@ function answer() {
 	var ans = $("#inp").val();
 	if (ans == "Корова из-за угла выглядывает." || ans == "Корова из-за угла выглядывает" || ans == "Корова из за угла выглядывает." || ans == "Корова из за угла выглядывает" || ans == "корова из-за угла выглядывает" || ans == "корова из за угла выглядывает" || ans == "корова из-за угла выглядывает." || ans == "корова из за угла выглядывает.") {
 		$("#quest").text("Ответ правильный!");
-		$("body").animate({
-			backgroundColor: "royalblue"
-		}, 500);
+		$("body").animate({backgroundColor: "royalblue"}, 500);
 	}
 	else {
 		$("#quest").text("Ответ неправильный!"); 
-		$("body").animate({
-			backgroundColor: "red"
-		}, 500);
+		$("body").animate({backgroundColor: "red"}, 500);
 	}
 	$("#inp").val("");
 }
