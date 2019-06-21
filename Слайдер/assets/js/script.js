375401ef1ee7735e7952b9d0d986c96f819d0ef9
Renat626 <kasymov.2002@mail.ru>
2019-06-21 21:20:46 +0500

    Обновление

--- Слайдер/assets/js/script.js
+++ Слайдер/assets/js/script.js
@@ -1,17 +1,25 @@
 /*sliderLeft()
 sliderRight()*/
+autoSliderLeft()
 
 document.getElementById("slider-left").onclick = sliderLeft;
 document.getElementById("slider-right").onclick = sliderRight;
 var left = 0;
+var timer;
+
+function autoSliderLeft() {
+	timer = setTimeout(sliderLeft, 1000);
+}
 
 function sliderLeft() {
 	var contant = document.getElementById("contant");
 	left = left - 800;
 	if(left < -4000) {
 		left = 0;
+		clearTimeout(timer);
 	}
 	contant.style.left = left + "px";
+	autoSliderLeft ();
 }
 
 function sliderRight() {
@@ -21,4 +29,13 @@ function sliderRight() {
 		left = -4000;
 	}
 	contant.style.left = left + "px";
+}
+
+/*Скругление*/
+
+document.getElementById("renat").oninput = borderRadius;
+
+function borderRadius() {
+	var div = document.getElementById("test");
+	div.style.borderRadius = this.value + "px";
 }
