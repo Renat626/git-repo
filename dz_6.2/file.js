76af01b44b7951f6272acc9392d5f4c5b99f1d6f
Slogur <ugadchicow44@gmail.com>
2019-08-12 15:30:09 +0500

    sss

--- dz_6.2/file.js
+++ dz_6.2/file.js
@@ -1,5 +1,13 @@
-var items = 0, price = 0;
-function buy(cost) {
-	items++; allItems.textContent = "Количество товаров: "+items+". ";
-	price += cost; allPrice.textContent = "Стоимость: "+price+"р.";
+let items = 0, price = 0;
+function listener() {
+	document.addEventListener('click', event => this.checkClick(event));
+	this.render();
 }
+function checkClick(event) {
+	if (event.target.tagName !== 'BUTTON') {return}
+   	else {
+   		items++; allItems.textContent = "Количество товаров: "+items+". ";
+		price += parseInt(event.target.dataset.cost); allPrice.textContent = "Стоимость: "+price+"р.";
+   	}
+}
+window.onload = () => listener();
