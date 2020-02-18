5be622f9049e640110da3cfb0bc41037859dec43
Slogur <ugadchicow44@gmail.com>
2020-02-18 13:34:15 +0500

    Menu animation

--- website/js.js
+++ website/js.js
@@ -1,5 +1,7 @@
 onscroll = function(e) {
 	if (window.scrollY > document.getElementById("header__main__contant").offsetHeight) {
-		document.getElementById("header__menu_mini").style.display = "inline";
-	} else {document.getElementById("header__menu_mini").style.display = "none"}
+		document.getElementById("header__menu_mini").style.top = "0px";
+	} else {
+		document.getElementById("header__menu_mini").style.top = "-100px";
+	}
 }
