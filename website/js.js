dd7161704cb14bd4a96aa639c3b02ca9d1f709d0
Renat626 <kasymov.2002@mail.ru>
2020-02-25 16:10:15 +0500

    Website

--- website/js.js
+++ website/js.js
@@ -5,3 +5,31 @@ onscroll = function(e) {
 		document.getElementById("header__menu_mini").style.top = "-100px";
 	}
 }
+
+//slider
+
+const buttonLeft = document.querySelector(".reviews__arrow__left");
+const buttonRight = document.querySelector(".reviews__arrow__right");
+let left = 0;
+let right = 0;
+
+buttonLeft.addEventListener('click', sliderLeft);
+buttonRight.addEventListener('click', sliderRight);
+
+function sliderLeft() {
+	const polosa = document.querySelector(".polosa");
+	left = left + 300;
+	if(left > 0) {
+		left = -600;
+	}
+	polosa.style.left = left + "px";
+}
+
+function sliderRight() {
+	const polosa = document.querySelector(".polosa");
+	right = right - 300;
+	if(right < -600) {
+		right = 0;
+	}
+	polosa.style.left = right + "px";
+}
