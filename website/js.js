df969ccacc2b4332d30abcc42d5c3d8a548fa9da
Slogur <ugadchicow44@gmail.com>
2020-03-04 18:41:46 +0500

    Ответы афигеные сделал

--- website/js.js
+++ website/js.js
@@ -1,10 +1,20 @@
-onscroll = function(e) {
+//menu
+
+onscroll = function() {
 	if (window.scrollY > document.getElementById("header__main__contant").offsetHeight) {
 		document.getElementById("header__menu_mini").style.top = "0px";
 	} else {
 		document.getElementById("header__menu_mini").style.top = "-100px";
 	}
 }
+window.onload = function() {
+	if (window.scrollY > document.getElementById("header__main__contant").offsetHeight) {
+		document.getElementById("header__menu_mini").style.top = "0px";
+	} else {
+		document.getElementById("header__menu_mini").style.top = "-100px";
+	}
+}
+
 
 //slider
 
@@ -35,6 +45,7 @@ function sliderRight() {
 	polosa.style.left = step + "px";
 }
 
+
 //header__burgerMenu
 
 const burgerButton = document.querySelector('#burger__button');
@@ -47,5 +58,31 @@ function showMenu() {
 		burgerMenu.style.display = "none";
 	} else {
 		burgerMenu.style.display = "block";
+	}
+}
+
+
+//answers
+
+const question_1 = document.querySelector('#question_1');
+const question_2 = document.querySelector('#question_2');
+const question_3 = document.querySelector('#question_3');
+
+question_1.addEventListener('click', function(){showAnswer(question_1, answer_1)});
+question_2.addEventListener('click', function(){showAnswer(question_2, answer_2)});
+question_3.addEventListener('click', function(){showAnswer(question_3, answer_3)});
+
+function showAnswer(que, ans, hei) {
+	if (que.offsetHeight != 54) {
+		que.style.height = "50px";
+		ans.style.visibility = "hidden";
+		ans.style.transition = "0.25s";
+		ans.style.opacity = 0;
+	} 
+	else {
+		que.style.height = ans.offsetHeight+60+"px";
+		ans.style.visibility = "visible";
+		ans.style.transition = "0.8s";
+		ans.style.opacity = 1;
 	}
 }
